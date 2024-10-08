e778352ac2597bc4601093b18cf03a42278ce274
Aaron Saray <me@aaronsaray.com>
2024-10-08 14:16:48 -0500

    Updates to support this loading faster. and to use PWM for LED.

--- boot.py
+++ boot.py
@@ -1,63 +1,17 @@
-from machine import Pin
-import json
-import network
-import esp
-import gc
-import asyncio
-import os
+from machine import Pin, PWM
 
-async def loading_led():
-  while True:
-    led.on()
-    await asyncio.sleep(0.1)
-    led.off()
-    await asyncio.sleep(0.1)
+def led_loading():
+    status_led_pwm.freq(1)
 
+def led_action():
+    status_led_pwm.freq(10)
 
-async def success_led():
-  led.on()
-  await asyncio.sleep(3)
-  led.off()
+def led_done():
+    status_led_pwm.deinit()
 
+def debug(item):
+    print(item)
 
-async def error_led():
-  led.on()
-  await asyncio.sleep(5)
-  led.off()
+status_led_pwm = PWM(Pin(2, Pin.OUT))
 
-
-async def connect_to_wifi(ssid, password):
-  wlan = network.WLAN(network.STA_IF)
-  wlan.active(True)
-  wlan.connect(ssid, password)
-
-  while not wlan.isconnected():
-    await asyncio.sleep(0.1)
-
-  print('WLAN Connection successful')
-  print(wlan.ifconfig())
-
-
-async def main():
-  esp.osdebug(None)
-  gc.collect()
-
-  loading_task = asyncio.create_task(loading_led())
-
-  if settingsFile in os.listdir():
-    file = open(settingsFile, 'r')
-    c = json.load(file)
-
-    await connect_to_wifi(c['ssid'], c['password'])
-
-    loading_task.cancel()
-    await success_led()
-  else:
-    loading_task.cancel()
-    await error_led()
-
-
-led = Pin(2, Pin.OUT)
-settingsFile = 'bedjet.json'
-
-asyncio.run(main())
+led_loading()
--- main.py
+++ main.py
@@ -3,46 +3,89 @@ try:
 except:
   import socket
 
+import json
+import network
+import esp
+import gc
+import os
+from time import sleep
 
-def web_page():
+settingsFile = 'bedjet.json'
+
+
+
+def connect_to_wifi(ssid, password):
+  wlan = network.WLAN(network.STA_IF)
+  wlan.active(True)
+  wlan.connect(ssid, password)
+
+  while not wlan.isconnected():
+    sleep(0.1)
+
+  debug('Wifi Connected')
+  debug(wlan.ifconfig())
+
+
+
+def get_web_response():
   with open("index.html", "r") as index:
     html = index.read()
     index.close()
-
   return html
 
-try:
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  s.bind(('', 80))
-  s.listen(5)
-except OSError as e:
-  print('failed')
-  print(e)
 
-while True:
+
+def web_server():
   try:
-    if gc.mem_free() < 102000:
-      gc.collect()
-    conn, addr = s.accept()
-    conn.settimeout(3.0)
-    print('Got a connection from %s' % str(addr))
-    request = conn.recv(1024)
-    conn.settimeout(None)
-    request = str(request)
-    print('Content = %s' % request)
-    led_on = request.find('/?led=on')
-    led_off = request.find('/?led=off')
-    if led_on == 6:
-      print('LED ON')
-    if led_off == 6:
-      print('LED OFF')
-    response = web_page()
-    conn.send('HTTP/1.1 200 OK\n')
-    conn.send('Content-Type: text/html\n')
-    conn.send('Connection: close\n\n')
-    conn.sendall(response)
-    conn.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', 80))
+    s.listen(5)
   except OSError as e:
-    conn.close()
-    print('Connection closed')
+    debug('Failed to open socket')
+    debug(e)
+    return
+
+  while True:
+    try:
+      if gc.mem_free() < 102000:
+        gc.collect()
+      conn, addr = s.accept()
+      conn.settimeout(3.0)
+
+      request = conn.recv(1024)
+      conn.settimeout(None)
+      request = str(request)
+
+      debug('Incoming request')
+      debug(request)
+
+      response = get_web_response()
+      conn.send('HTTP/1.1 200 OK\n')
+      conn.send('Content-Type: text/html\n')
+      conn.send('Connection: close\n\n')
+      conn.sendall(response)
+      conn.close()
+    except OSError as e:
+      conn.close()
+      debug('Connection closed error')
+      debug(e)
+
+
+
+def main():
+  esp.osdebug(None)
+  gc.collect()
+
+  if settingsFile in os.listdir():
+    led_action()
+    file = open(settingsFile, 'r')
+    c = json.load(file)
+
+    connect_to_wifi(c['ssid'], c['password'])
+
+  led_done()
+
+  web_server()
+
+main()
